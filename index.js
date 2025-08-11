291e62d4411204af3b571e45a06b744644d3dd40
Ken VanDine <ken@vandine.org>
2025-08-11 11:46:22 -0400

    feat: Handle zoom with keyboard and mousewheel

--- index.js
+++ index.js
@@ -67,6 +67,23 @@ function createWindow () {
     win.hide();
   });
 
+  ipcMain.on('zoom-in', () => {
+    console.log('zoom-in');
+    const currentZoom = win.webContents.getZoomLevel();
+    win.webContents.setZoomLevel(currentZoom + 1);
+  });
+
+  ipcMain.on('zoom-out', () => {
+    console.log('zoom-out');
+    const currentZoom = win.webContents.getZoomLevel();
+    win.webContents.setZoomLevel(currentZoom - 1);
+  });
+
+  ipcMain.on('zoom-reset', () => {
+    console.log('zoom-reset');
+    win.webContents.setZoomLevel(0);
+  });
+
   ipcMain.on('log-message', (event, message) => {
     console.log('Log from preload: ', message);
   });
